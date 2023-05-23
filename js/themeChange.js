7f0d123276caa8f7a2d434f094db81b4f9119703
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-05-23 16:14:59 +0200

    update base.php

--- js/themeChange.js
+++ js/themeChange.js
@@ -3,22 +3,22 @@ window.addEventListener("load", function(){
     let logoImg = document.getElementById('logoImg');
     let logoBig = document.getElementById('logoBig');
     changeTheme();
-  });
+});
 
-  function changeTheme(){
+function changeTheme(){
     if (mode !== undefined && mode === 'dark'){
-      //logoImg.src = "upload/logos/NetGamers_Logo_White.png";
-      logoBig.src = "upload/logos/NetGamers_Logo_White.png";
-      mode = 'light';
+        //logoImg.src = "upload/logos/NetGamers_Logo_White.png";
+        logoBig.src = "upload/logos/NetGamers_Logo_White.png";
+        mode = 'light';
     } else {
-      //logoImg.src = "upload/logos/NetGamers_Logo.png";
-      logoBig.src = "upload/logos/NetGamers_Logo.png";
-      mode = 'dark';
+        //logoImg.src = "upload/logos/NetGamers_Logo.png";
+        logoBig.src = "upload/logos/NetGamers_Logo.png";
+        mode = 'dark';
     }
-
+    
     if (themeCheck.checked == true){ //dark theme
-      //logoImg.src = "upload/logos/NetGamers_Logo_White.png";
+        //logoImg.src = "upload/logos/NetGamers_Logo_White.png";
     } else { //light theme
-      //logoImg.src = "upload/logos/NetGamers_Logo.png";        
+        //logoImg.src = "upload/logos/NetGamers_Logo.png";        
     }
-  }
+}
--- template/base.php
+++ template/base.php
@@ -1,175 +1,170 @@
 <!DOCTYPE html>
 <html lang="it">
-<head>
-    <meta charset="utf-8">
-    <title><?php echo $templateParams["titolo"]; ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <head>
+        <meta charset="utf-8">
+        <title><?php echo $templateParams["titolo"]; ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <link rel="icon" type="image/png" sizes="32x32" href="upload/logos/NetGamers_Icon.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="upload/logos/NetGamers_Icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="upload/logos/NetGamers_Icon.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="upload/logos/NetGamers_Icon.png">
 
-    <meta name="theme-color" content="white">
-    <!-- Theme mode-->
-    <script>
-      let mode = window.localStorage.getItem('mode'),
-          root = document.getElementsByTagName('html')[0];
-      /*if (mode === 'dark'){
-        document.getElementById("logoImg").src = "upload/logos/NetGamers_Logo_White.png";
-      }
-      if (mode !== 'dark'){
-        document.getElementById("logoImg").src = "upload/logos/NetGamers_Logo.png";        
-      }*/
-      if (mode !== undefined && mode === 'dark') {
-        root.classList.add('dark-mode');
-      } else {
-        root.classList.remove('dark-mode');
-      }
-    </script>
+        <meta name="theme-color" content="white">
+        <!-- Theme mode-->
+        <script>
+        let mode = window.localStorage.getItem('mode'),
+            root = document.getElementsByTagName('html')[0];
+        if (mode !== undefined && mode === 'dark') {
+            root.classList.add('dark-mode');
+        } else {
+            root.classList.remove('dark-mode');
+        }
+        </script>
 
-    <!-- Page loading styles-->
-    <style>
-      .page-loading {
-        position: fixed;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        -webkit-transition: all .4s .2s ease-in-out;
-        transition: all .4s .2s ease-in-out;
-        background-color: #fff;
-        opacity: 0;
-        visibility: hidden;
-        z-index: 9999;
-      }
-      .dark-mode .page-loading {
-        background-color: #121519;
-      }
-      .page-loading.active {
-        opacity: 1;
-        visibility: visible;
-      }
-      .page-loading-inner {
-        position: absolute;
-        top: 50%;
-        left: 0;
-        width: 100%;
-        text-align: center;
-        -webkit-transform: translateY(-50%);
-        transform: translateY(-50%);
-        -webkit-transition: opacity .2s ease-in-out;
-        transition: opacity .2s ease-in-out;
-        opacity: 0;
-      }
-      .page-loading.active > .page-loading-inner {
-        opacity: 1;
-      }
-      .page-loading-inner > span {
-        display: block;
-        font-family: 'Inter', sans-serif;
-        font-size: 1rem;
-        font-weight: normal;
-        color: #6f788b;
-      }
-      .dark-mode .page-loading-inner > span {
-        color: #fff;
-        opacity: .6;
-      }
-      .page-spinner {
-        display: inline-block;
-        width: 2.75rem;
-        height: 2.75rem;
-        margin-bottom: .75rem;
-        vertical-align: text-bottom;
-        background-color: #d7dde2; 
-        border-radius: 50%;
-        opacity: 0;
-        -webkit-animation: spinner .75s linear infinite;
-        animation: spinner .75s linear infinite;
-      }
-      .dark-mode .page-spinner {
-        background-color: rgba(255,255,255,.25);
-      }
-      @-webkit-keyframes spinner {
-        0% {
-          -webkit-transform: scale(0);
-          transform: scale(0);
+        <!-- Page loading styles-->
+        <style>
+        .page-loading {
+            position: fixed;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            -webkit-transition: all .4s .2s ease-in-out;
+            transition: all .4s .2s ease-in-out;
+            background-color: #fff;
+            opacity: 0;
+            visibility: hidden;
+            z-index: 9999;
         }
-        50% {
-          opacity: 1;
-          -webkit-transform: none;
-          transform: none;
+        .dark-mode .page-loading {
+            background-color: #121519;
         }
-      }
-      @keyframes spinner {
-        0% {
-          -webkit-transform: scale(0);
-          transform: scale(0);
+        .page-loading.active {
+            opacity: 1;
+            visibility: visible;
         }
-        50% {
-          opacity: 1;
-          -webkit-transform: none;
-          transform: none;
+        .page-loading-inner {
+            position: absolute;
+            top: 50%;
+            left: 0;
+            width: 100%;
+            text-align: center;
+            -webkit-transform: translateY(-50%);
+            transform: translateY(-50%);
+            -webkit-transition: opacity .2s ease-in-out;
+            transition: opacity .2s ease-in-out;
+            opacity: 0;
         }
-      }
-      
-    </style>
-    <!-- Page loading scripts-->
-    <script>
-      (function () {
-        window.onload = function () {
-          const preloader = document.querySelector('.page-loading');
-          preloader.classList.remove('active');
-          setTimeout(function () {
-            preloader.remove();
-          }, 1500);
-        };
-      })();
-    </script>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&amp;display=swap" rel="stylesheet" id="google-font">
-    <link rel="stylesheet" media="screen" href="css/theme.min.css">
-  </head>
-  <body>
-    <div class="page-loading active">
-      <div class="page-loading-inner">
-        <div class="page-spinner"></div><span>Loading...</span>
-      </div>
-    </div>
-
-    <main class="page-wrapper">
-    <header class="navbar navbar-expand-lg fixed-top">
-        <div class="container">
-          <a class="navbar-brand pe-sm-3" href="index.php"><span class="text-primary flex-shrink-0 me-2">
-              <img id="logoImg" src="upload/logos/NetGamers_Icon.png" style="height: 50px;" alt="" />
-          </a>
-          <div class="form-check form-switch mode-switch order-lg-2 me-3 me-lg-4 ms-auto" data-bs-toggle="mode">
-            <input class="form-check-input" type="checkbox" id="theme-mode" onclick="changeTheme()">
-            <label class="form-check-label" for="theme-mode"><i class="ai-sun fs-lg"></i></label>
-            <label class="form-check-label" for="theme-mode"><i class="ai-moon fs-lg"></i></label>
-          </div>
-          <?php if(isset($_SESSION["username"])){
-            require("loggedin-menu.php");
-          } ?>
-        </div>
-      </header>
-      <!-- Page content-->
-        <div class="container py-5 mt-4 mt-lg-5 mb-lg-4 my-xl-5">
-          <div class="row pt-sm-2 pt-lg-0 align-items-center">
-            <?php
-            if(isset($templateParams["nome"])){
-                require($templateParams["nome"]);
+        .page-loading.active > .page-loading-inner {
+            opacity: 1;
+        }
+        .page-loading-inner > span {
+            display: block;
+            font-family: 'Inter', sans-serif;
+            font-size: 1rem;
+            font-weight: normal;
+            color: #6f788b;
+        }
+        .dark-mode .page-loading-inner > span {
+            color: #fff;
+            opacity: .6;
+        }
+        .page-spinner {
+            display: inline-block;
+            width: 2.75rem;
+            height: 2.75rem;
+            margin-bottom: .75rem;
+            vertical-align: text-bottom;
+            background-color: #d7dde2; 
+            border-radius: 50%;
+            opacity: 0;
+            -webkit-animation: spinner .75s linear infinite;
+            animation: spinner .75s linear infinite;
+        }
+        .dark-mode .page-spinner {
+            background-color: rgba(255,255,255,.25);
+        }
+        @-webkit-keyframes spinner {
+            0% {
+            -webkit-transform: scale(0);
+            transform: scale(0);
             }
-            ?>
-          </div>
+            50% {
+            opacity: 1;
+            -webkit-transform: none;
+            transform: none;
+            }
+        }
+        @keyframes spinner {
+            0% {
+            -webkit-transform: scale(0);
+            transform: scale(0);
+            }
+            50% {
+            opacity: 1;
+            -webkit-transform: none;
+            transform: none;
+            }
+        }
+        
+        </style>
+        <!-- Page loading scripts-->
+        <script>
+        (function () {
+            window.onload = function () {
+            const preloader = document.querySelector('.page-loading');
+            preloader.classList.remove('active');
+            setTimeout(function () {
+                preloader.remove();
+            }, 1500);
+            };
+        })();
+        </script>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&amp;display=swap" rel="stylesheet" id="google-font">
+        <link rel="stylesheet" media="screen" href="css/theme.min.css">
+    </head>
+    <body>
+        <div class="page-loading active">
+        <div class="page-loading-inner">
+            <div class="page-spinner"></div><span>Loading...</span>
         </div>
-    </main>
-    <!-- Vendor scripts: js libraries and plugins-->
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
-    <!-- Main theme script-->
-    <script src="js/theme.min.js"></script>
-    <!-- theme changer script -->
-    <script src="js/themeChange.js"></script>
-  </body>
+        </div>
+
+        <main class="page-wrapper">
+            <header class="navbar navbar-expand-lg fixed-top">
+                <div class="container">
+                    <a class="navbar-brand pe-sm-3" href="index.php"><span class="text-primary flex-shrink-0 me-2">
+                        <img id="logoImg" src="upload/logos/NetGamers_Icon.png" style="height: 50px;" alt="" />
+                    </a>
+                    <div class="form-check form-switch mode-switch order-lg-2 me-3 me-lg-4 ms-auto" data-bs-toggle="mode">
+                        <input class="form-check-input" type="checkbox" id="theme-mode" onclick="changeTheme()">
+                        <label class="form-check-label" for="theme-mode"><i class="ai-sun fs-lg"></i></label>
+                        <label class="form-check-label" for="theme-mode"><i class="ai-moon fs-lg"></i></label>
+                    </div>
+                    <?php if(isset($_SESSION["username"])){
+                        require("loggedin-menu.php");
+                    } 
+                    ?>
+                </div>
+            </header>
+            <!-- Page content-->
+            <div class="container py-5 mt-4 mt-lg-5 mb-lg-4 my-xl-5">
+                <div class="row pt-sm-2 pt-lg-0 align-items-center">
+                    <?php
+                    if(isset($templateParams["nome"])){
+                        require($templateParams["nome"]);
+                    }
+                    ?>
+                </div>
+            </div>
+        </main>
+        <!-- Vendor scripts: js libraries and plugins-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
+        <!-- Main theme script-->
+        <script src="js/theme.min.js"></script>
+        <!-- theme changer script -->
+        <script src="js/themeChange.js"></script>
+    </body>
 </html>
