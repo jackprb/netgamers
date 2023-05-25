03969e37ec4ae2493bf6901f720cb853ff449182
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-05-25 13:38:49 +0200

    add index.php

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
-echo "ciao";
+require_once 'CONFIG.php';
+
+//if(isUserLoggedIn()){
+    $templateParams["titolo"] = "NetGamers - Feed";
+    $templateParams["nome"] = "feed.php";
+    if(isset($_GET["formmsg"])){
+        $templateParams["formmsg"] = $_GET["formmsg"];
+    }
+//} else {
+   // header("location:login.php?a=1");
+//}
+
+require 'template/base.php';
 
 ?>
