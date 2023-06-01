4976eef23beafb9375ac5be915e47fc7b352cfb4
Andrea Dotti <andrea.dotti4@studio.unibo.it>
2023-06-01 20:06:38 +0200

    Added some query to database.php

--- db/database.php
+++ db/database.php
@@ -19,7 +19,128 @@ class DatabaseHelper{
 
         return $result->fetch_all(MYSQLI_ASSOC);
     }   
+
+    public function getUserEmail($username){
+        $query = "SELECT email FROM users WHERE active=1 AND username = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('s', $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }  
+
+    public function insertNewPost($userId, $img, $title, $text){
+        $query = "INSERT INTO posts(img, title, `text`, dateTimePublished, userID) VALUES (?, ?, ?, ?, ?)";
+        $stmt = $this->db->prepare($query);
+        $img = 'helloooo';
+        $stmt->bind_param('sssss', $img, $title, $text, date('Y-m-d H:i:s'), $userId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $stmt->errno;
+    }
+
+    public function newComment($userId, $postId, $img, $text){
+        $query = "INSERT INTO comments VALUES (?, ?, ?, ?, ?)";
+        $stmt = $this->db->prepare($query);
+        $img = 'helloooo';
+        $stmt->bind_param('sssss', $img, $text, date('Y-m-d H:i:s'), $userId, $postId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $stmt->errno;
+    }
     
+    public function modifyComment($userId, $postId, $img, $text){
+        $query = "UPDATE comments SET img = ?, text = ?, dateTime = ? WHERE userID = ? AND postID = ?";
+        $stmt = $this->db->prepare($query);
+        $img = 'helloooo';
+        $stmt->bind_param('sssss', $img, $text, date('Y-m-d H:i:s'), $userId, $postId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $stmt->errno;
+    }
+
+    public function newFollow($userIdFollowing, $userIdFollowed){
+        $query = "INSERT INTO follow VALUES (?, ?)";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('ss', $userIdFollowing, $userIdFollowed);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $stmt->errno;
+    }
+
+    public function removeFollow($userIdFollowing, $userIdFollowed){
+        $query = "DELETE FROM follow WHERE userFollowing = ? AND userFollowed = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('ss', $userIdFollowing, $userIdFollowed);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $stmt->errno;
+    }
+
+    public function getFollowers($userIdFollowed){
+        $query = "SELECT userFollowing FROM follow WHERE userFollowed = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('s', $userIdFollowed);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $stmt->errno;
+    }
+
+    public function getFollowed($userIdFollowing){
+        $query = "SELECT userFollowed FROM follow WHERE userFollowing = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('s', $userIdFollowing);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $stmt->errno;
+    }
+
+    public function updatePost($postId, $userId, $img, $title, $text){
+        $query = "UPDATE posts SET img = ?, title = ?, `text` = ?, dateTimePublished = ? WHERE ID = ?";
+        $stmt = $this->db->prepare($query);
+        $img = 'helloooo';
+        $stmt->bind_param('sssss', $img, $title, $text, date('Y-m-d H:i:s'), $postId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $stmt->errno;
+    } 
+
+    public function updateUserEmail($username, $email){
+        $query = "UPDATE users SET email = ? WHERE username = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('ss', $email, $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $stmt->errno;
+    }   
+
+    public function standbyAccount($username){
+        $query = "UPDATE users SET active = 0 WHERE username = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('s', $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $stmt->errno;
+    }   
+
+    public function getUserImg($username){
+        $query = "SELECT userImg FROM users WHERE active=1 AND username = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('s', $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }   
+    
+    public function updateUserImg($username, $imgPath){
+        $query = "UPDATE users SET userImg = ? WHERE username = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('ss', $imgPath, $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $stmt->errno;
+    }   
+
     public function getCurrentUserPsw($username){
         $query = "SELECT psw FROM users WHERE active=1 AND username = ?";
         $stmt = $this->db->prepare($query);
