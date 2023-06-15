a9b19e8e69058e4902dd4b5b87cd859d9c4ea9de
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-06-15 17:41:59 +0200

    update search page - search feature now working

--- template/single-post-footer.php
+++ template/single-post-footer.php
@@ -1,24 +1,17 @@
                 <div class="card-footer">
                     <div class="row">
-                        <div class="col-8">
-                            <p class="fs-lg nav-item p-2">
-                                <a href="#">#GameName </a> or <a href="#">#postTopic</a>
-                            </p>
-                        </div>
-                        <div class="col-4">
-                            <div class="d-flex justify-content-end">
-                                <?php if($hasImage) : ?>
-                                <a href="#" class="nav-item position-relative fs-4 p-2 mx-sm-1" data-bs-toggle="modal" data-bs-target="#modalText" >
-                                    <i class="ai-note"></i>
-                                </a>
-                                <?php endif; ?>
-                                <a href="#" class="nav-item position-relative fs-4 p-2 mx-sm-1">
-                                    <i class="ai-message-plus"></i>
-                                </a>
-                                <a href="#" class="nav-item position-relative fs-4 p-2 mx-sm-1">
-                                    <i class="ai-heart"></i>
-                                </a>
-                            </div>
+                        <div class="d-flex justify-content-end">
+                            <?php if($hasImage) : ?>
+                            <a href="#" class="nav-item position-relative fs-4 p-2 mx-sm-1" data-bs-toggle="modal" data-bs-target="#modalText" >
+                                <i class="ai-note"></i>
+                            </a>
+                            <?php endif; ?>
+                            <a href="#" class="nav-item position-relative fs-4 p-2 mx-sm-1">
+                                <i class="ai-message-plus"></i>
+                            </a>
+                            <a href="#" class="nav-item position-relative fs-4 p-2 mx-sm-1">
+                                <i class="ai-heart"></i>
+                            </a>
                         </div>
                     </div>   
                 </div>
