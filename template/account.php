c278ac085789905c6cec580d5f35403b8f2972e2
Michele <micheleandrenacci97@gmail.com>
2023-06-15 15:12:16 +0200

    Add lenguages in account

--- template/account.php
+++ template/account.php
@@ -119,6 +119,26 @@
                                 <option value="" selected disabled>Select your main language</option>
                                 <option value="English">English</option>
                                 <option value="Italiano">Italiano</option>
+                                <option value="Espanol">Español</option>
+                                <option value="Francais">Français</option>
+                                <option value="Deutsch">Deutsch</option>
+                                <option value="Portugues">Português</option>
+                                <option value="Nederlands">Nederlands</option>
+                                <option value="Russian">Русский</option>
+                                <option value="Giapponese">日本語</option>
+                                <option value="CineseS">简体中文</option>
+                                <option value="Coreano">한국어</option>
+                                <option value="Arabo">العربية</option>
+                                <option value="Svenska">Svenska</option>
+                                <option value="Norsk">Norsk</option>
+                                <option value="Dansk">Dansk</option>
+                                <option value="Suomi">Suomi</option>
+                                <option value="Polski">Polski</option>
+                                <option value="Ceco">Čeština</option>
+                                <option value="Magyar">Magyar</option>
+                                <option value="Rumeno">Română</option>
+                                <option value="Greco">Ελληνικά</option>
+                                <option value="Turco">Türkçe</option>
                             </select>
                         </div>
                         <div class="col-12 col-sm-12 col-md-6">
