253f6f4731f2e0580f7525fe11e57e778b23243a
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-06-21 12:07:47 +0200

    update account.php

--- template/account.php
+++ template/account.php
@@ -271,13 +271,7 @@
                                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                 <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
                             </svg>
-                            <p class="mb-0">Password must be minimum 10 characters long and must contain at least 
-                                <ul>
-                                    <li>a number</li>
-                                    <li>a special char and</li>
-                                    <li>a capital letter.</li>
-                                </ul>
-                            </p>
+                            <p class="mb-0">Password <strong>must</strong> be minimum 10 characters long and <strong>must</strong> contain at least one number, one special char and one capital letter.</p>
                         </div>
                         <div class="d-flex justify-content-end pt-3">
                             <input type="submit" class="btn btn-primary ms-3" value="Save changes" />
