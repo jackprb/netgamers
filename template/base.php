9c935f10f34cb7dc5f9e111c9a8309d0a2a7b6be
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-06-24 11:35:08 +0200

    fix accessibility issues

--- template/base.php
+++ template/base.php
@@ -140,6 +140,7 @@
         </div>
         
         <main class="page-wrapper">
+            <?php if(!str_contains(basename($_SERVER['PHP_SELF']), "login.php")): ?>
             <div class="offcanvas offcanvas-end" id="offcanvasPost" tabindex="-1">
                 <div class="offcanvas-header border-bottom">
                     <h5 class="offcanvas-title">New posts</h5>
@@ -217,6 +218,7 @@
                     </ul>
                 </div>
             </div>
+            <?php endif; ?>
             
             <header class="navbar navbar-expand-sm fixed-top">
                 <div class="container">
@@ -225,7 +227,7 @@
                             <span class="navbar-toggler-icon"></span>
                         </button>
                         <a href="index.php">
-                            <img class="logoImg img-fluid" src="upload/logos/NetGamers_Icon.png" alt="" />
+                            <img class="logoImg img-fluid" src="upload/logos/NetGamers_Icon.png" alt="Netgamers Icon" />
                         </a>
                     </div>
                     <div class="nav align-items-center order-sm-2">
@@ -256,14 +258,15 @@
                         </div>
                         <?php endif; ?>
                         <a class="nav-link p-1" href="#">
-                            <div class="form-check form-switch mode-switch ms-auto" data-bs-toggle="mode">
+                            <div class="form-check form-switch mode-switch" data-bs-toggle="mode">
+                                <label class="d-none" for="theme-mode">Switch theme mode</label>
                                 <input class="form-check-input" type="checkbox" id="theme-mode" onclick="changeTheme()">
-                                <label class="form-check-label" for="theme-mode">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-sun text-dark svg-navbar" viewBox="0 0 16 16" role="img" aria-label="Light theme">
+                                <label class="form-check-label">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-sun text-dark me-2 svg-navbar" viewBox="0 0 16 16" role="img" aria-label="Light theme">
                                         <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
                                     </svg>
                                 </label>
-                                <label class="form-check-label" for="theme-mode">
+                                <label class="form-check-label">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-moon text-dark svg-navbar" viewBox="0 0 16 16" role="img" aria-label="Dark theme">
                                         <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z"/>
                                     </svg>
@@ -305,7 +308,7 @@
                             </li>
                         </ul>
                     </nav>
-                    <?php endif; ?>
+                    <?php endif; ?> 
                 </div>
             </header>
 
@@ -324,7 +327,7 @@
             <div class="container pt-3 pt-sm-2 pt-md-3 pt-lg-4">
                 <div class="border-bottom text-center pb-4">
                     <a class="navbar-brand d-inline-flex text-nav py-0 mb-1" href="index.php">
-                        <img class="logoBig" src="upload/logos/NetGamers_Logo_White.png" alt=""/>
+                        <img class="logoBig" src="upload/logos/NetGamers_Logo_White.png" alt="Netgamers logo"/>
                     </a>
                 </div>
                 <div class="d-sm-flex align-items-center justify-content-between pt-4 mt-2 mt-sm-0">
--- template/login-form.php
+++ template/login-form.php
@@ -23,72 +23,72 @@
         }
     }
 ?>
-<!-- Sign in form-->
-<div class="d-flex justify-content-center align-items-center mb-4">
-    <img class="logoBig" src="" style="height: 140px;" alt="" />
-</div>
-<div class="col-md-6 mb-5 mb-md-0">
-    <div class="card dark-mode border-0 bg-primary py-md-3 py-lg-4 px-lg-4 px-xl-5">
-        <div class="card-body">
-            <h1 class="py-2 pb-lg-3">Log in</h1>
+    <!-- Sign in form-->
+    <div class="d-flex justify-content-center align-items-center mb-4">
+        <img class="logoBig" src="upload/logos/NetGamers_Logo_White.png" alt="Netgamers logo" style="height: 140px;" />
+    </div>
+    <div class="col-md-6 mb-5 mb-md-0">
+        <div class="card dark-mode border-0 bg-primary py-md-3 py-lg-4 px-lg-4 px-xl-5">
+            <div class="card-body">
+                <h1 class="py-2 pb-lg-3">Log in</h1>
 
-            <?php if(isset($_GET)): ?>
-                <p class="fw-semibold"><?php echo getLoginMsg(); ?></p>
+                <?php if(isset($_GET)): ?>
+                    <p class="fw-semibold"><?php echo getLoginMsg(); ?></p>
+                <?php endif; ?>
+
+                <form action="./login_process.php" method="POST" autocomplete="off">
+                    <div class="pb-3 mb-3">
+                        <label class="form-label d-none" for="username">Username</label>
+                        <input class="form-control form-control-lg" type="text" name="username" id="username" placeholder="Username" required>
+                    </div>
+                    <div class="password-toggle mb-4">
+                        <label class="form-label d-none" for="password">Password</label>
+                        <input class="form-control form-control-lg" type="password" placeholder="Password" id="password" name="password" required>
+                    </div>
+                    <button class="btn btn-lg btn-light w-100 mb-4" type="submit" name="login">Log in</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-6 col-xl-5 offset-xl-1">
+        <div class="ps-md-3 ps-lg-5 ps-xl-0">
+            <h2 class="h1 pb-2 pb-lg-3">No account? Sign Up</h2>
+
+            <?php if(isset($_GET["r"]) && $_GET["r"] == 1): ?>
+                <p class="fw-semibold">Registration completed successfully! Please log in.</p>
+            <?php endif; ?>
+            <?php if(isset($_GET["er"]) && $_GET["er"] >= 1 && $_GET["er"] <= 5): ?>
+                <p class="fw-semibold"><?php echo getRegisterMSg($_GET["er"]); ?></p>
             <?php endif; ?>
 
-            <form action="./login_process.php" method="POST" autocomplete="off">
-                <div class="pb-3 mb-3">
-                    <label class="form-label d-none" for="username">Username</label>
-                    <input class="form-control form-control-lg" type="text" name="username" id="username" placeholder="Username" required>
+            <form action="./register_process.php" method="POST" autocomplete="off">
+                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-1 row-cols-lg-2">
+                    <div class="col mb-4">
+                        <label class="form-label d-none" for="usernameReg">Username</label>
+                        <input class="form-control form-control-lg" type="text" placeholder="Username" required name="usernameReg" id="usernameReg">
+                    </div>
+                    <div class="col mb-4">
+                        <label class="form-label d-none" for="email">Email address</label>
+                        <input class="form-control form-control-lg" type="email" placeholder="Email address" required name="email" id="email">
+                    </div>
                 </div>
                 <div class="password-toggle mb-4">
-                    <label class="form-label d-none" for="password">Password</label>
-                    <input class="form-control form-control-lg" type="password" placeholder="Password" id="password" name="password" required>
+                    <label class="form-label d-none" for="psw">Password</label>
+                    <input class="form-control form-control-lg" type="password" placeholder="Password" required name="psw" id="psw">
                 </div>
-                <button class="btn btn-lg btn-light w-100 mb-4" type="submit" name="login">Log in</button>
+                <div class="password-toggle mb-4">
+                    <label class="form-label d-none" for="confPsw">Confirm password</label>
+                    <input class="form-control form-control-lg" type="password" placeholder="Confirm password" required name="confPsw" id="confPsw">
+                </div>
+                <div class="alert alert-info d-flex my-3 my-sm-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-info-circle text-dark me-2 svg-md align-self-start" viewBox="0 0 16 16" aria-hidden="true">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                        <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                    </svg>
+                    <p class="mb-0">Password <strong>must</strong> be minimum 10 characters long and <strong>must</strong> contain at least one number, one special char and one capital letter.</p>
+                </div>
+                <button class="btn btn-lg btn-primary w-100 mb-4" type="submit" name="signup">Sign up</button>
             </form>
         </div>
     </div>
-</div>
-
-<div class="col-md-6 col-xl-5 offset-xl-1">
-    <div class="ps-md-3 ps-lg-5 ps-xl-0">
-        <h2 class="h1 pb-2 pb-lg-3">No account? Sign Up</h2>
-
-        <?php if(isset($_GET["r"]) && $_GET["r"] == 1): ?>
-            <p class="fw-semibold">Registration completed successfully! Please log in.</p>
-        <?php endif; ?>
-        <?php if(isset($_GET["er"]) && $_GET["er"] >= 1 && $_GET["er"] <= 5): ?>
-            <p class="fw-semibold"><?php echo getRegisterMSg($_GET["er"]); ?></p>
-        <?php endif; ?>
-
-        <form action="./register_process.php" method="POST" autocomplete="off">
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-1 row-cols-lg-2">
-                <div class="col mb-4">
-                    <label class="form-label d-none" for="usernameReg">Username</label>
-                    <input class="form-control form-control-lg" type="text" placeholder="Username" required name="usernameReg" id="usernameReg">
-                </div>
-                <div class="col mb-4">
-                    <label class="form-label d-none" for="email">Email address</label>
-                    <input class="form-control form-control-lg" type="email" placeholder="Email address" required name="email" id="email">
-                </div>
-            </div>
-            <div class="password-toggle mb-4">
-                <label class="form-label d-none" for="psw">Password</label>
-                <input class="form-control form-control-lg" type="password" placeholder="Password" required name="psw" id="psw">
-            </div>
-            <div class="password-toggle mb-4">
-                <label class="form-label d-none" for="confPsw">Confirm password</label>
-                <input class="form-control form-control-lg" type="password" placeholder="Confirm password" required name="confPsw" id="confPsw">
-            </div>
-            <div class="alert alert-info d-flex my-3 my-sm-4">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-info-circle text-dark me-2 svg-md align-self-start" viewBox="0 0 16 16" aria-hidden="true">
-                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                    <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-                </svg>
-                <p class="mb-0">Password <strong>must</strong> be minimum 10 characters long and <strong>must</strong> contain at least one number, one special char and one capital letter.</p>
-            </div>
-            <button class="btn btn-lg btn-primary w-100 mb-4" type="submit" name="signup">Sign up</button>
-        </form>
-    </div>
-</div>
