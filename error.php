9e89f1d22c0e1a6a611c291c173c004573666f23
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-07-04 17:35:13 +0200

    add missing requirements

--- error.php
+++ error.php
@@ -3,6 +3,8 @@ require_once 'CONFIG.php';
 
 $templateParams["titolo"] = "NetGamers - Error";
 $templateParams["nome"] = "error.php";
+$templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js","./api/js/get_notifications.js",
+            "./api/js/read_notification.js");
 
 require 'template/base.php';
 
